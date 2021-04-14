930e4fbc6383230599aab059fbc4a65228cc04a5
magam <magamed773377@gmail.com>
2021-04-14 17:11:11 +0300

    Начало Добавил формы регистрации и авторизации

---
+++ forms/users.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
+
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+
+    submit = SubmitField('Регистрация')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
